d4b52dba90592c01824b405bf6b7770e149c155b
slasher <gdiaz.pabloignacio@gmail.com>
2024-01-30 15:17:05 -0300

    add middleware y funciones de control de errores

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,7 +12,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "Todos los campos son requeridos" });
+    next(errorHandler(400, "Todos los campos son requeridos"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +27,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("Registro exitoso");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
---
+++ api/utils/error.js
@@ -0,0 +1,6 @@
+export const errorHanlder = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
