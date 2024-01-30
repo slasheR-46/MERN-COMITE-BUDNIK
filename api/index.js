cf65e8e09848b45da3e1f8a21a3be4615b0e31de
slasheR-46 <gdiaz.pabloignacio@gmail.com>
2024-01-30 00:39:13 -0300

    conexion a database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Success: MongoDb esta conectado");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Servidor levantado y corriendo en el puerto 3000");
+  console.log("Success: Servidor corriendo en puerto 3000");
 });
