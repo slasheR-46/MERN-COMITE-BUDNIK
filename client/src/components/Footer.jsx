c89f478ab00d71ec052c6f28f1b45e4c49ebc37e
slasher <gdiaz.pabloignacio@gmail.com>
2024-01-31 16:49:04 -0300

    avance creacion footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,71 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-orange-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mg-5">
+            <Link
+              to="/"
+              className=" self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-orange-600 via-orange-400 to-orange-600 rounded-lg text-white">
+                Comite
+              </span>
+              Budnik
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.budnik.cl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Budnik
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Siguenos" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.budnik.cl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.budnik.cl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Legal</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <Footer.Copyright
+            href="#"
+            by="Budnik - Ti"
+            year={new Date().getFullYear()}
+          />
+        </div>
+      </div>
+    </Footer>
+  );
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -44,7 +44,10 @@ export default function SignUp() {
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
-          <Link to="/" className=" font-bold dark:text-white text-4xl">
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:thext white"
+          >
             <span className="px-2 py-1 bg-gradient-to-r from-orange-600 via-orange-400 to-orange-600 rounded-lg text-white">
               Comite
             </span>
