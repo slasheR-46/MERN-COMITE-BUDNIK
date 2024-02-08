3050371646f9d9c07624a06aa82157eb463ce743
slasher <gdiaz.pabloignacio@gmail.com>
2024-02-08 18:18:20 -0300

    Dark mode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
 import persistStore from "redux-persist/es/persistStore";
 import storage from "redux-persist/lib/storage";
+import themeReducer from "./theme/themeSlice";
 import userReducer from "./user/userSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
