cf275f2701540089f5e385251191c019059ea435
slasher <gdiaz.pabloignacio@gmail.com>
2024-02-13 18:25:01 -0300

    avance agregar ruta de API de usuario de actualización

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,42 @@
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
   res.json({ message: "Api: levantada" });
+};
+
+export const updateUser = async (req, res) => {
+  if (req.user.id !== req.params.userId) {
+    return next(
+      errorHandler(403, "No tienes permiso para actualizar este usuario.")
+    );
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "No estas autorizado para esta ruta"));
+    }
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(
+          400,
+          "El nombre de usuario debe tener entre 7 y 20 caracteres."
+        )
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(
+        errorHandler(400, "El nombre de usuario no puede contener espacios")
+      );
+    }
+  }
+  if (req.body.username !== req.user.username.toLowerCase()) {
+    return next(
+      errorHandler(400, "El nombre de usuario debe estar en minúsculas")
+    );
+  }
+  // if(!req.body.username.match(/^[a-z0-9]+$/)) {
+  //   return next(errorHandler(400, "El nombre de usuario solo puede contener letras y números"));
+  // }
 };
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.acces_token;
+  if (!token) {
+    return next(errorHandler(401, "No estas autorizado para esta ruta"));
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return next(errorHandler(401, "No estas autorizado para esta ruta"));
+    }
+    req.user = user;
+    next();
+  });
+};
