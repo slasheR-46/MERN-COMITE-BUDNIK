e3e1f13653e7b73c0a1aaf6c3558b828f3b32f2c
slasher <gdiaz.pabloignacio@gmail.com>
2024-02-19 18:16:36 -0300

    correcion useController

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,7 +4,7 @@ export const test = (req, res) => {
   res.json({ message: "Api: levantada" });
 };
 
-export const updateUser = async (req, res) => {
+export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(
       errorHandler(403, "No tienes permiso para actualizar este usuario.")
@@ -30,13 +30,36 @@ export const updateUser = async (req, res) => {
         errorHandler(400, "El nombre de usuario no puede contener espacios")
       );
     }
+    if (req.body.username !== req.user.username.toLowerCase()) {
+      return next(
+        errorHandler(400, "El nombre de usuario debe estar en minúsculas")
+      );
+    }
+    if (!req.body.username.match(/^[a-z0-9]+$/)) {
+      return next(
+        errorHandler(
+          400,
+          "El nombre de usuario solo puede contener letras y números"
+        )
+      );
+    }
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            profilePicture: req.body.profilePicture,
+            password: req.body.password,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updateUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
   }
-  if (req.body.username !== req.user.username.toLowerCase()) {
-    return next(
-      errorHandler(400, "El nombre de usuario debe estar en minúsculas")
-    );
-  }
-  // if(!req.body.username.match(/^[a-z0-9]+$/)) {
-  //   return next(errorHandler(400, "El nombre de usuario solo puede contener letras y números"));
-  // }
 };
